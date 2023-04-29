cb33cf9af5567d94200863caab4c28b13fbf29b5
ChaelPix <55618696+ChaelPix@users.noreply.github.com>
2023-04-29 13:13:07 +0200

    random words

--- englishvoccards/script.js
+++ englishvoccards/script.js
@@ -1,61 +1,65 @@
-let words = [];
-let currentCard = 0;
-const card = document.getElementById('definition');
-const answerInput = document.getElementById('answer');
-const answerContainer = document.getElementById('answerContainer');
-const form = document.querySelector('form');
-const submitButton = document.querySelector('button');
-const nextButton = document.getElementById('nextButton');
+fetch("words.json")
+    .then((response) => response.json())
+    .then((data) => {
+        let words = data;
 
-// Charger les données à partir du fichier JSON
-fetch('words.json')
-    .then(response => response.json())
-    .then(data => {
-        words = data;
+        let currentCard = 0;
+        const card = document.getElementById("definition");
+        const answerInput = document.getElementById("answer");
+        const answerContainer = document.getElementById("answerContainer");
+        const nextButton = document.getElementById("nextButton");
+        const form = document.querySelector("form");
+
         card.textContent = words[currentCard].definition;
+
+        function getRandomCardIndex() {
+            return Math.floor(Math.random() * words.length);
+        }
+
+        function checkAnswer() {
+            if (answerInput.value.trim().toLowerCase() === words[currentCard].word) {
+                card.classList.remove("incorrect");
+                card.classList.add("correct");
+                answerContainer.textContent = "";
+                answerContainer.style.display = "none";
+                answerInput.disabled = true;
+                setTimeout(function () {
+                    nextCard();
+                }, 1000);
+            } else {
+                card.classList.remove("correct");
+                card.classList.add("incorrect");
+                answerContainer.textContent =
+                    "La réponse correcte est : " + words[currentCard].word;
+                answerContainer.style.display = "block";
+                answerInput.disabled = true;
+            }
+            nextButton.style.display = "block";
+        }
+
+        function nextCard() {
+            currentCard = getRandomCardIndex();
+            card.classList.remove("incorrect");
+            card.classList.remove("correct");
+            answerContainer.textContent = "";
+            answerContainer.style.display = "none";
+            card.textContent = words[currentCard].definition;
+            answerInput.value = "";
+            answerInput.disabled = false;
+            nextButton.style.display = "none";
+        }
+
+        nextButton.addEventListener("click", nextCard);
+
+        // Ajoutez cet événement pour éviter la soumission du formulaire
+        form.addEventListener("submit", function (event) {
+            event.preventDefault();
+            checkAnswer();
+        });
+
+        nextButton.addEventListener("click", nextCard);
+        nextCard();
+    })
+    .catch((error) => {
+        console.error("Error fetching words:", error);
     });
-
-card.textContent = words[currentCard].definition;
-
-function checkAnswer() {
-    if (answerInput.value.trim().toLowerCase() === words[currentCard].word) {
-        card.classList.remove("incorrect");
-        card.classList.add("correct");
-        submitButton.style.display = "none";
-        nextButton.style.display = "block";
-        answerContainer.textContent = '';
-        answerContainer.style.display = 'none';
-        answerInput.disabled = true;
-        setTimeout(function () {
-            nextCard();
-        }, 1000);
-    } else {
-        card.classList.remove("correct");
-        card.classList.add("incorrect");
-        answerContainer.textContent = 'La réponse correcte est : ' + words[currentCard].word;
-        answerContainer.style.display = 'block';
-        submitButton.style.display = "none";
-        nextButton.style.display = "block";
-        answerInput.disabled = true;
-    }
-}
-
-function nextCard() {
-    currentCard = (currentCard + 1) % words.length;
-    card.classList.remove("incorrect");
-    card.classList.remove("correct");
-    answerContainer.textContent = '';
-    answerContainer.style.display = 'none';
-    card.textContent = words[currentCard].definition;
-    answerInput.value = '';
-    answerInput.disabled = false;
-    submitButton.style.display = "block";
-    nextButton.style.display = "none";
-}
-
-form.addEventListener("submit", function (event) {
-    event.preventDefault();
-    checkAnswer();
-});
-
-nextButton.addEventListener("click", nextCard);

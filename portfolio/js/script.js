9e8eaad6cb12a022588319138d971830d4ccf8d9
ChaelPix <55618696+ChaelPix@users.noreply.github.com>
2023-08-19 18:39:56 +0200

    mouse follow

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -23,3 +23,37 @@ window.addEventListener('resize', function() {
   });
 });
 
+//Follow Mouse
+const follower = document.getElementById('follower');
+let mouseX = 0, mouseY = 0, posX = 0, posY = 0;
+
+document.addEventListener('mousemove', (e) => {
+    mouseX = e.clientX - follower.offsetWidth / 2;
+    mouseY = e.clientY - follower.offsetHeight / 2;
+    follower.style.display = 'block'; 
+});
+
+function animate() {
+  posX += (mouseX - posX) * 0.25;
+  posY += (mouseY - posY) * 0.25;
+  posY -= 35;
+  follower.style.transform = `translate(${posX}px, ${posY}px)`;
+
+  requestAnimationFrame(animate);
+}
+
+function isTouchDevice() {
+  return 'ontouchstart' in window || navigator.maxTouchPoints;
+}
+
+if (isTouchDevice()) {
+  console.log("C'est un appareil tactile!");
+} else {
+  console.log("Ce n'est pas un appareil tactile.");
+  animate();
+}
+
+
+
+
+

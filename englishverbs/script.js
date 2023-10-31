414b0b3b72ca27276af7f457280f0026b474825e
ChaelPix <55618696+ChaelPix@users.noreply.github.com>
2023-10-31 22:46:21 +0100

    Revert before pull merge
    This reverts commit cb34dad2224f76754dc0208c049a8fb0e3165da2.

--- englishverbs/script.js
+++ englishverbs/script.js
@@ -113,141 +113,113 @@ const irregularVerbs = [
     ["wear", "wore", "worn", "porter (un vêtement)"],
     ["win", "won", "won", "gagner"],
     ["write", "wrote", "written", "écrire"]
-    ];
-    
-    let currentVerbs = [];
-    let isValidation = false;
-    
-    console.log("Nombre de verbes irréguliers :", irregularVerbs.length);
-    function generateRow(verb) {
-        const row = document.createElement("tr");
-        const randomIndexToFill = Math.floor(Math.random() * 4); 
-    
-        for (let i = 0; i < 4; i++) {
-            const cell = document.createElement("td");
-            if (i === randomIndexToFill) {
-                cell.textContent = verb[i];
-            } else {
-                const input = document.createElement("input");
-                input.type = "text";
-                input.dataset.index = i;
-                cell.appendChild(input);
-            }
-            row.appendChild(cell);
-        }
-    
-        return row;
-    }
-    
-    
-    function fillTable() {
-        const table = document.getElementById("irregularVerbs");
-        currentVerbs = [];
-      
-        // Créez une copie du tableau des verbes irréguliers
-        const remainingVerbs = [...irregularVerbs];
-      
-        for (let i = 0; i < 10; i++) {
-          // Sélectionnez un verbe aléatoire parmi les verbes restants
-          const randomIndex = Math.floor(Math.random() * remainingVerbs.length);
-          const selectedVerb = remainingVerbs[randomIndex];
-      
-          // Ajoutez le verbe sélectionné au tableau des verbes actuels
-          currentVerbs.push(selectedVerb);
-      
-          // Supprimez le verbe sélectionné du tableau des verbes restants
-          remainingVerbs.splice(randomIndex, 1);
-      
-          // Générez et ajoutez la nouvelle ligne au tableau
-          const newRow = generateRow(selectedVerb);
-          table.appendChild(newRow);
-        }
-      }
-      
-    
-    function resetTable() {
-        const table = document.getElementById("irregularVerbs");
-    
-        while (table.rows.length > 1) {
-            table.deleteRow(1);
-        }
-    
-        fillTable();
-    }
-    
-    function removeAccents(str) {
-        return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-      }
-    
-    function checkAnswer(input, answer) {
-        const mainAnswer = answer.split(" (")[0];
-        input = input.split(" (")[0];
-        return removeAccents(input.toLowerCase()) === removeAccents(mainAnswer.toLowerCase());
-      }
-      
-    
-    
-      function validate() {
-          const inputs = document.querySelectorAll("input");
-          let correctCount = 0;
-      
-          inputs.forEach((input) => {
-              const row = input.parentElement.parentElement;
-              const rowIndex = row.rowIndex - 1;
-              const columnIndex = parseInt(input.dataset.index);
-      
-              if (checkAnswer(input.value.trim(), currentVerbs[rowIndex][columnIndex])) {
-                  input.classList.add("correct");
-                  correctCount++;
-              } else {
-                  input.classList.add("incorrect");
-                  const correctAnswer = document.createElement("span");
-                  correctAnswer.textContent = currentVerbs[rowIndex][columnIndex];
-                  correctAnswer.classList.add("correct-answer");
-                  input.parentElement.appendChild(correctAnswer);
-              }
-          });
-      
-          const scoreDisplay = document.getElementById("score");
-          correctCount = (correctCount / 30 * 20);
+];
 
-          // Créez un élément span pour le chiffre correctCount
-          const correctCountSpan = document.createElement("span");
-          correctCountSpan.style.color = "red";
-          
-          // Vérifiez si correctCount a une décimale égale à zéro
-          if (correctCount % 1 === 0) {
-            correctCountSpan.textContent = correctCount.toFixed(0); // Pas de décimales
-          } else {
-            correctCountSpan.textContent = correctCount.toFixed(1); // Une décimale si nécessaire
-          }
-          
-          // Créez un élément span pour la partie "/20."
-          const outOfSpan = document.createElement("span");
-          outOfSpan.textContent = "/20.";
-          outOfSpan.style.color = "red";
-          
-          // Ajoutez les éléments span à la fin du texte
-          scoreDisplay.textContent = "Vous avez reçu la note de ";
-          scoreDisplay.appendChild(correctCountSpan);
-          scoreDisplay.appendChild(outOfSpan);
-          
-          const actionButton = document.getElementById("actionButton");
-          actionButton.textContent = "Recommencer";
-          isValidation = true;
-      }
-    
-    document.getElementById("actionButton").addEventListener("click", () => {
-        if (isValidation) {
-            resetTable();
-            document.getElementById("score").textContent = "";
-            document.getElementById("actionButton").textContent = "Valider";
-            //document.getElementById("actionButton").textContent = window.location.reload();
-            isValidation = false;
+let currentVerbs = [];
+let isValidation = false;
+
+console.log("Nombre de verbes irréguliers :", irregularVerbs.length);
+function generateRow(verb) {
+    const row = document.createElement("tr");
+    const randomIndexToFill = Math.floor(Math.random() * 4);
+
+    for (let i = 0; i < 4; i++) {
+        const cell = document.createElement("td");
+        if (i === randomIndexToFill) {
+            cell.textContent = verb[i];
         } else {
-            validate();
+            const input = document.createElement("input");
+            input.type = "text";
+            input.dataset.index = i;
+            cell.appendChild(input);
+        }
+        row.appendChild(cell);
+    }
+
+    return row;
+}
+
+
+function fillTable() {
+    const table = document.getElementById("irregularVerbs");
+    currentVerbs = [];
+
+    const remainingVerbs = [...irregularVerbs];
+
+    for (let i = 0; i < 10; i++) {
+        const randomIndex = Math.floor(Math.random() * remainingVerbs.length);
+        const selectedVerb = remainingVerbs[randomIndex];
+
+        currentVerbs.push(selectedVerb);
+
+        remainingVerbs.splice(randomIndex, 1);
+
+        const newRow = generateRow(selectedVerb);
+        table.appendChild(newRow);
+    }
+}
+
+
+function resetTable() {
+    const table = document.getElementById("irregularVerbs");
+
+    while (table.rows.length > 1) {
+        table.deleteRow(1);
+    }
+
+    fillTable();
+}
+
+function removeAccents(str) {
+    return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+}
+
+function checkAnswer(input, answer) {
+    const mainAnswer = answer.split(" (")[0];
+    input = input.split(" (")[0];
+    return removeAccents(input.toLowerCase()) === removeAccents(mainAnswer.toLowerCase());
+}
+
+
+
+function validate() {
+    const inputs = document.querySelectorAll("input");
+    let correctCount = 0;
+
+    inputs.forEach((input) => {
+        const row = input.parentElement.parentElement;
+        const rowIndex = row.rowIndex - 1;
+        const columnIndex = parseInt(input.dataset.index);
+
+        if (checkAnswer(input.value.trim(), currentVerbs[rowIndex][columnIndex])) {
+            input.classList.add("correct");
+            correctCount++;
+        } else {
+            input.classList.add("incorrect");
+            const correctAnswer = document.createElement("span");
+            correctAnswer.textContent = currentVerbs[rowIndex][columnIndex];
+            correctAnswer.classList.add("correct-answer");
+            input.parentElement.appendChild(correctAnswer);
         }
     });
-    
-    fillTable();
-    frameLooper();
+
+    const scoreDisplay = document.getElementById("score");
+    scoreDisplay.textContent = `Votre score est de ${correctCount} sur ${inputs.length}.`;
+
+    const actionButton = document.getElementById("actionButton");
+    actionButton.textContent = "Recommencer";
+    isValidation = true;
+}
+
+document.getElementById("actionButton").addEventListener("click", () => {
+    if (isValidation) {
+        resetTable();
+        document.getElementById("score").textContent = "";
+        document.getElementById("actionButton").textContent = "Valider";
+        isValidation = false;
+    } else {
+        validate();
+    }
+});
+
+fillTable();

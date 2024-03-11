5a50dd0545272668af911347ffa8cd898aed3259
ChaelPix <chael.pixel@gmail.com>
2024-03-11 17:42:59 +0100

    regional page

--- portfolio/js/carousel.js
+++ portfolio/js/carousel.js
@@ -7,22 +7,20 @@ function updateArrowVisibility() {
     const nextArrow = document.querySelector('.carousel-next');
 
     if (currentIndex === 0) {
-        prevArrow.style.display = 'none'; // Cache la flèche gauche si sur la première page
+        prevArrow.style.display = 'none'; 
     } else {
-        prevArrow.style.display = 'block'; // Affiche la flèche gauche si pas sur la première page
+        prevArrow.style.display = 'block'; 
     }
 
     if (currentIndex === totalPages - 1) {
-        nextArrow.style.display = 'none'; // Cache la flèche droite si sur la dernière page
+        nextArrow.style.display = 'none'; 
     } else {
-        nextArrow.style.display = 'block'; // Affiche la flèche droite si pas sur la dernière page
+        nextArrow.style.display = 'block'; 
     }
 }
 
-// Fonction pour ajouter l'animation fade-in aux éléments
 function fadeInElements(page) {
-    // Sélection de tous les éléments à animer
-    const elementsToFadeIn = page.querySelectorAll('h2, .carousel-images img, .carousel-description');
+    const elementsToFadeIn = page.querySelectorAll('h2, .carousel-images img, .carousel-description, p');
 
     elementsToFadeIn.forEach((element, index) => {
         element.style.opacity = 0;
@@ -43,22 +41,19 @@ document.querySelector('.carousel-prev').addEventListener('click', function() {
 });
 
 function changePage(n) {
-    // Retirer la classe fade-in de tous les éléments de la page actuelle
     const currentPage = pages[currentIndex];
-    const elementsToFadeOut = currentPage.querySelectorAll('h2, .carousel-images img, .carousel-description');
+    const elementsToFadeOut = currentPage.querySelectorAll('h2, .carousel-images img, .carousel-description, p');
     elementsToFadeOut.forEach(element => element.style.opacity = 0);
     
     currentIndex = (currentIndex + n + totalPages) % totalPages;
 
-    // Sélectionner la nouvelle page et y appliquer fade-in
     const newPage = pages[currentIndex];
-    fadeInElements(newPage); // Appel de la fonction pour animer les éléments
+    fadeInElements(newPage); 
 
     document.querySelector('.carousel-container').style.transform = `translateX(-${currentIndex * 100}%)`;
     updateArrowVisibility();
 }
 
-// Initialise le carrousel pour afficher la première page avec l'animation
 document.addEventListener("DOMContentLoaded", () => {
     fadeInElements(pages[currentIndex]);
 });

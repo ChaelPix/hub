d9a63d5f5fa5b920b075646760c94bb951aa1557
ChaelPix <55618696+ChaelPix@users.noreply.github.com>
2024-05-25 21:37:14 +0200

    test no post process

--- test_web_a/ServiceWorker.js
+++ test_web_a/ServiceWorker.js
@@ -1,6 +1,33 @@
+const cacheName = "DefaultCompany-test_web-0.1.0";
+const contentToCache = [
+    "Build/test_web_a.loader.js",
+    "Build/test_web_a.framework.js.unityweb",
+    "Build/test_web_a.data.unityweb",
+    "Build/test_web_a.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
     
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
 });
 
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});

3bb416c9e79114add05fd5f19154ce8d318ee963
ChaelPix <chael.pixel@gmail.com>
2024-09-22 11:27:59 +0200

    design tweaks

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Pix'
+project = 'Mes Cours'
 copyright = '2024'
 author = ' '
 
@@ -42,7 +42,7 @@ templates_path = ['_templates']
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = 'fr'
+language = 'eng'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.

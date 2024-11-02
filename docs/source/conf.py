2490c7ca69911f61ab62bfddd97f90ac378850ff
ChaelPix <chael.pixel@gmail.com>
2024-11-02 22:28:50 +0100

    added md support

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,6 +31,7 @@ extensions = [
     'sphinx_rtd_theme',
     'sphinx_copybutton',
     'sphinxcontrib.video',
+    'myst_parser',
 ]
 
 
@@ -66,3 +67,9 @@ html_static_path = ['_static']
 html_css_files = [
     'css/custom.css',
 ]
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}

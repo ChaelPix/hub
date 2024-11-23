70388227b6901a9bb5cad089352aa63840ff88fd
ChaelPix <chael.pixel@gmail.com>
2024-11-23 18:04:59 +0100

    responsive banner

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./*.html"],
   theme: {
-    extend: {}, 
+    extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["nord", "night"],
+  },
 };
+
+
 
